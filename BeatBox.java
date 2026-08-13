d6a6ad88f62261159602882729804149ece7ddcc
Girija Prasanna <girijaprasanna1999@gmail.com>
2026-08-13 14:33:49 +0530

    - Use default name "youNameless" when no argument is provided - Prevent ArrayIndexOutOfBoundsException when starting without arguments

--- BeatBox.java
+++ BeatBox.java
@@ -45,7 +45,8 @@ public class BeatBox {
     int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 
     public static void main(String[] args) {
-        new BeatBox().startUp(args[0]);
+        String name = args.length > 0 ? args[0] : "youNameless";
+        new BeatBox().startUp(name);
     }
 
     public void startUp(String name) {
